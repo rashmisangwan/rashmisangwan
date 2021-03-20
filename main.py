84480c9863a65ba7b3809a11e5729858ae6d1ca7
rashmi <errashmisangwan@gmail.com>
2021-03-20 08:41:04 +0530

    testing application flow

--- main.py
+++ main.py
@@ -19,18 +19,8 @@ def index():
     return render_template('home.html')
 
 
-@app.route('/about')
-def about_handler():
-    return '<h1> about </h1>'
-
-
-@app.route('/contact')
-def contact_handler():
-    return '<h1> contact </h1>'
-
-
 @app.route('/api/projects/basic_project', methods=['GET'])
-def project_handler():
+def project_api_handler():
     a = request.args.to_dict()
     print(a)
     b = list(a.values())
@@ -64,6 +54,46 @@ def project_handler():
                     'output': {"drug_suggested": str(model.predict(X)[0])}
                     })
     # return { 'name': request.args.get('name') }
+
+
+
+@app.route('/projects/basic_project', methods=['GET'])
+def project_handler():
+    a = request.args.to_dict()
+    print(a)
+    b = list(a.values())
+    c = []
+    for i in range(len(b)):
+        if i == 0:
+            c.append(int(b[i]))
+        elif i == 4:
+            c.append(float(b[i]))
+        else:
+            c.append(b[i])
+    X = np.array([c], dtype='object')
+
+    from sklearn import preprocessing
+    le_sex = preprocessing.LabelEncoder()
+    le_sex.fit(['F', 'M'])
+    X[:, 1] = le_sex.transform(X[:, 1])
+
+    le_BP = preprocessing.LabelEncoder()
+    le_BP.fit(['LOW', 'NORMAL', 'HIGH'])
+    X[:, 2] = le_BP.transform(X[:, 2])
+
+    le_Chol = preprocessing.LabelEncoder()
+    le_Chol.fit(['NORMAL', 'HIGH'])
+    X[:, 3] = le_Chol.transform(X[:, 3])
+    
+    print(X, "rashmi")
+    print("your drug is : ", model.predict(X))
+
+    # return jsonify({'input': {'age': a['age'], 'sex': a['sex'], "BP": a['BP'], "colestrol": a['Cholesterol'], "na_to_k": a['Na_to_K']},
+    #                 'output': {"drug_suggested": str(model.predict(X)[0])}
+    #                 })
+    # return { 'name': request.args.get('name') }
+
+    return render_template('project_basic.html')
 
 
 if __name__ == "__main__":
