b6f63bf7b7016138b18181991b6628005ec7871d
rashmi <errashmisangwan@gmail.com>
2021-03-21 10:05:26 +0530

    added procfile and port mentioned in app

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+import os
 from app import create_app 
 
 if __name__ == '__main__':
     _app = create_app()
-    _app.run(host = "0.0.0.0", port= 8080, debug = True)
+    port = int(os.environ.get('PORT', 8080))
+    _app.run(host = "0.0.0.0", port = port, debug = True)
     
     
