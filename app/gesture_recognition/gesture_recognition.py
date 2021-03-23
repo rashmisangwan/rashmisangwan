8d0ce69accd1348efc02f4a0df53074c720e362d
rashmi <errashmisangwan@gmail.com>
2021-03-24 00:19:39 +0530

    route placeholder

--- app/gesture_recognition/gesture_recognition.py
+++ app/gesture_recognition/gesture_recognition.py
@@ -4,6 +4,9 @@ bp = Blueprint('gesture_recognition_bp', __name__, template_folder='templates')
 
 
 @bp.route('/api/projects/gesture_recognition', methods=['GET'])
-def gesture_handler():
-    
-    return "hello..welcome back"
+def api_route():
+    return jsonify({'status': 'in_progress'})
+
+@bp.route('/projects/gesture_recognition', methods=['GET'])
+def app_route():
+    return "status :: in progress"
