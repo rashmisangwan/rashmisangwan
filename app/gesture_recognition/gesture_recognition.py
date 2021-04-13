52202b90adfd2d26df41622706d0705efeeea274
rashmi <errashmisangwan@gmail.com>
2021-04-14 00:00:49 +0530

    added web cam support for gesture recognition

--- app/gesture_recognition/gesture_recognition.py
+++ app/gesture_recognition/gesture_recognition.py
@@ -4,12 +4,25 @@ import os
 import base64
 from flask import Blueprint, render_template, request, jsonify, current_app
 from werkzeug.utils import secure_filename
+from flask_socketio import emit, join_room, leave_room
+
+from .. import socketio
 
 bp = Blueprint('gesture_recognition_bp', __name__, template_folder='templates')
 
 @bp.route('/api/projects/gesture_recognition', methods=['GET'])
 def api_route():
-    return jsonify({'status': 'in_progress'})
+    f = request.files['formFile']
+    file_path = os.path.join(current_app.config['UPLOAD_FOLDER'], secure_filename(f.filename))
+    f.save(file_path)
+
+    output = model_runner.run(file_path)
+    f.seek(0)
+
+    encoded = base64.b64encode(f.read()).decode()
+    return jsonify(
+        {'input': 'data:image/png;base64,{}'.format(encoded), 'output': output})
+
 
 @bp.route('/projects/gesture_recognition', methods=['GET', 'POST'])
 def app_route():
@@ -28,3 +41,17 @@ def app_route():
         return render_template('gesture_recognition.html', data={'input': 'data:image/png;base64,{}'.format(encoded), 'output': output})
     else:
         return render_template('gesture_recognition.html')
+
+from PIL import Image
+from base64 import decodestring
+
+@socketio.on('image_frame')
+def image_frame(data):
+    decoded = base64.b64decode(data)
+    decodedBytes = BytesIO(decoded)
+
+    im = Image.open(decodedBytes)
+
+    im.save('image.png', 'PNG')
+    output = model_runner.run('image.png')
+    emit('response_back', str(output[0]))  ## ??
--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import os
-from app import create_app 
+from app import create_app, socketio
 
 if __name__ == '__main__':
     _app = create_app()
     _app.config['UPLOAD_FOLDER'] = 'uploads'
     
     port = int(os.environ.get('PORT', 8080))
-    _app.run(host = "0.0.0.0", port = port, debug = True)
+    #_app.run(host = "0.0.0.0", port = port, debug = True)
+    socketio.run(_app, host= "0.0.0.0", port = port, debug = True)
+
     
     
