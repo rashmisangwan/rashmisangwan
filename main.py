fc2acc0440f89d5e6218a28f777d6e36106f86f7
Rashmi Sangwan <errashmisangwan@gmail.com>
2021-04-14 16:05:32 +0530

    again syntax error

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def Application(config, gunicornInstance):
     
     # port = int(os.environ.get('PORT', 8080))
     # _app.run(host = "0.0.0.0", port = port, debug = True)
-    socketio.run(_app, host= config.SERVER_NAME or "0.0.0.0", config.SERVER_NAME or 8080, debug = True)
+    socketio.run(_app, host = config.SERVER_NAME or "0.0.0.0", port = config.SERVER_NAME or 8080, debug = True)
 
 if __name__ == '__main__':
     Application()
