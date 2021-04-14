bd441333ff71f4323fe9dd856ac277b6ef0299b9
Rashmi Sangwan <errashmisangwan@gmail.com>
2021-04-14 15:56:16 +0530

    Heroku Gunicorn test code

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 import os
 from app import create_app, socketio
 
-def Application(*args):
-    print(args)
+def Application(config, gunicornInstance):
     _app = create_app()
     _app.config['UPLOAD_FOLDER'] = 'uploads'
     
-    port = int(os.environ.get('PORT', 8080))
-    #_app.run(host = "0.0.0.0", port = port, debug = True)
-    socketio.run(_app, host= "0.0.0.0", port = port, debug = True)
+    # port = int(os.environ.get('PORT', 8080))
+    # _app.run(host = "0.0.0.0", port = port, debug = True)
+    socketio.run(_app, host= config.SERVER_NAME || "0.0.0.0", config.SERVER_NAME || 8080, debug = True)
 
 if __name__ == '__main__':
     Application()
